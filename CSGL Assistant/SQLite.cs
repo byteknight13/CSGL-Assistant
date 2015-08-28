a8835d9658e0aa2ad86fdf1b964595d4c65c7226
byteknight13 <runedev@gmail.com>
2015-08-28 14:01:35 -0700

    Added SQLite

--- CSGL Assistant/SQLite.cs
+++ CSGL Assistant/SQLite.cs
@@ -5,14 +5,16 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSGL_Assistant.Properties;
 
 namespace CSGL_Assistant
 {
     // ReSharper disable once InconsistentNaming
-    class SQLite
+    class SQLiteAssistant
     {
-        public SQLiteConnection NewSqLiteConnection(string dbLocation)
+        public SQLiteConnection NewSqLiteConnection()
         {
+            string dbLocation = Settings.Default.DBLocation;
             var connString = String.Format("Data Source={0};Version=3;", dbLocation);
             return new SQLiteConnection(connString);
         }
---
+++ CSGL Assistant/frmMain.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CSGL_Assistant
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.themeManager = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.gridDB = new DevExpress.XtraGrid.GridControl();
+            this.gviewDB = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gviewDB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // themeManager
+            // 
+            this.themeManager.LookAndFeel.SkinName = "Sharp";
+            this.themeManager.LookAndFeel.TouchUIMode = DevExpress.LookAndFeel.TouchUIMode.False;
+            // 
+            // gridDB
+            // 
+            this.gridDB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridDB.Location = new System.Drawing.Point(12, 182);
+            this.gridDB.MainView = this.gviewDB;
+            this.gridDB.Name = "gridDB";
+            this.gridDB.Size = new System.Drawing.Size(456, 200);
+            this.gridDB.TabIndex = 0;
+            this.gridDB.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gviewDB});
+            // 
+            // gviewDB
+            // 
+            this.gviewDB.GridControl = this.gridDB;
+            this.gviewDB.Name = "gviewDB";
+            this.gviewDB.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
+            this.gviewDB.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
+            this.gviewDB.OptionsBehavior.Editable = false;
+            this.gviewDB.OptionsView.ShowGroupPanel = false;
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl1.Size = new System.Drawing.Size(684, 141);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 394);
+            this.Controls.Add(this.ribbonControl1);
+            this.Controls.Add(this.gridDB);
+            this.Name = "frmMain";
+            this.Text = "CSGL Assistant";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gviewDB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel themeManager;
+        private DevExpress.XtraGrid.GridControl gridDB;
+        private DevExpress.XtraGrid.Views.Grid.GridView gviewDB;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+
+    }
+}
+
