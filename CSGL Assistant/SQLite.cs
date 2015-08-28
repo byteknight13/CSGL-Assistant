95dd6ddb338bf652d93667b592cb3f34e8544ee0
byteknight13 <runedev@gmail.com>
2015-08-28 12:45:52 -0700

    Initial commit

---
+++ CSGL Assistant/SQLite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGL_Assistant
+{
+    // ReSharper disable once InconsistentNaming
+    class SQLite
+    {
+        public SQLiteConnection NewSqLiteConnection(string dbLocation)
+        {
+            var connString = String.Format("Data Source={0};Version=3;", dbLocation);
+            return new SQLiteConnection(connString);
+        }
+    }
+}
